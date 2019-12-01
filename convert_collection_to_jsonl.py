848a3c71a26c2bf889cf227e88880961d9e3371c
Rodrigo Frassetto Nogueira <rodrigonogueira4@gmail.com>
2019-12-01 13:25:09 -0500

    Update convert_collection_to_jsonl.py

--- convert_collection_to_jsonl.py
+++ convert_collection_to_jsonl.py
@@ -4,7 +4,27 @@ import os
 import argparse
 
 
-def convert_collection():
+def convert_collection(args):
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Converts MSMARCO\'s tsv collection to Anserini jsonl '
+                    'files.')
+    parser.add_argument('--collection_path', required=True,
+                        help='MS MARCO .tsv collection file')
+    parser.add_argument('--predictions', required=True,
+                        help='File containing predicted queries.')
+    parser.add_argument('--output_folder', required=True, help='output folder')
+    parser.add_argument('--max_docs_per_file', default=1000000, type=int,
+                        help='maximum number of documents in each jsonl file.')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output_folder):
+        os.makedirs(args.output_folder)
+
     print('Converting collection...')
 
     file_index = 0
@@ -35,22 +55,4 @@ def convert_collection():
                 print('Converted {} docs in {} files'.format(i, file_index))
 
     output_jsonl_file.close()
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Converts MSMARCO\'s tsv collection to Anserini jsonl '
-                    'files.')
-    parser.add_argument('--collection_path', required=True,
-                        help='MS MARCO .tsv collection file')
-    parser.add_argument('--output_folder', required=True, help='output folder')
-    parser.add_argument('--max_docs_per_file', default=1000000, type=int,
-                        help='maximum number of documents in each jsonl file.')
-
-    args = parser.parse_args()
-
-    if not os.path.exists(args.output_folder):
-        os.makedirs(args.output_folder)
-
-    convert_collection()
     print('Done!')
