371e408f5c9bbf51763dcd2a7bbc915af6154732
ronakice <ronakice@hotmail.com>
2021-11-11 02:37:33 -0500

    update d2q v1 doc

--- msmarco-v1/augment_corpus.py
+++ msmarco-v1/augment_corpus.py
@@ -32,7 +32,10 @@ def augment_corpus_with_doc2query_t5(dataset, searcher, f_out, num_queries, text
             output_dict = json.loads(searcher.doc(docid).raw())
         except:
             print(docid)
-        concatenated_queries = " ".join(dataset[i]["predicted_queries"][:num_queries])
+        if num_queries == -1:
+            concatenated_queries = " ".join(dataset[i]["predicted_queries"])
+        else:
+            concatenated_queries = " ".join(dataset[i]["predicted_queries"][:num_queries])
         output_dict[text_key] = f"{output_dict[text_key]} {concatenated_queries}"
         counter += 1
         output.write(json.dumps(output_dict) + '\n')  
@@ -45,15 +48,18 @@ if __name__ == '__main__':
         description='Concatenate MS MARCO V1 corpus with predicted queries')
     parser.add_argument('--hgf_d2q_dataset', required=True, 
                         choices=['castorini/msmarco_v1_passage_doc2query-t5_expansions',
-                        'castorini/msmarco_v1_doc_segmented_doc2query-t5_expansions'])
-    parser.add_argument('--prebuilt_index', required=True, help='Prebuilt index name',
-                        choices=['msmarco-passage', 'msmarco-doc-per-passage'])
+                        'castorini/msmarco_v1_doc_segmented_doc2query-t5_expansions',
+                        'castorini/msmarco_v1_doc_doc2query-t5_expansions'])
+    parser.add_argument('--prebuilt_index', required=True, help='Prebuilt index name')
     parser.add_argument('--output_psg_path', required=True, help='Output file for d2q-t5 augmented corpus.')
-    parser.add_argument('--num_queries', default=20, type=int, help='Number of expansions used.')
+    parser.add_argument('--num_queries', default=-1, type=int, help='Number of expansions used.')
     parser.add_argument('--cache_dir', default=".", type=str, help='Path to cache the hgf dataset')
     args = parser.parse_args()
 
     dataset = load_dataset(args.hgf_d2q_dataset, split="train", cache_dir=args.cache_dir)
-    searcher = SimpleSearcher.from_prebuilt_index(args.prebuilt_index)
+    if args.prebuilt_index in ['msmarco-passage', 'msmarco-doc-per-passage', 'msmarco-doc']:
+        searcher = SimpleSearcher.from_prebuilt_index(args.prebuilt_index)
+    else:
+        searcher = SimpleSearcher(args.prebuilt_index)
     augment_corpus_with_doc2query_t5(dataset, searcher, args.output_psg_path, args.num_queries)
     print('Done!')
